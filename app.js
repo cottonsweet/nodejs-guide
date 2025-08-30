d0f4b86ac1d4a3853e02c2906018fd61cd09af0e
lk1 <whitecandy777@naver.com>
2025-08-30 18:46:07 +0900

    feat: express route, middleware

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("first middleware");
+
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/products", (req, res, next) => {
   console.log("second middleware");
-  res.send("he");
+  res.send("프로덕트 페이지");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("first middleware");
+  res.send("루트 페이지");
 });
 
 app.listen(4000);
