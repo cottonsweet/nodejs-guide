5ce24ba3609df847fc12e2329da05a61f89e0596
lk1 <whitecandy777@naver.com>
2025-08-31 16:59:12 +0900

    feat: 데이터 공유 처리

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,15 @@ import rootDir from "../utils/path.js";
 
 const router = express.Router();
 
+export const products = [];
+
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
 import express from "express";
 import path from "path";
 import rootDir from "../utils/path.js";
+import { products } from "./admin.js";
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(products, "shop.js");
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
