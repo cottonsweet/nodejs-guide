3e77f71c0321fa496525c96c0685c158a72a263d
lk1 <whitecandy777@naver.com>
2025-09-14 21:11:46 +0900

    feat: mongoose 유저 추가

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// import User from "./models/user.js";
+import User from "./models/user.js";
 
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
@@ -27,44 +27,44 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   // 사용자 컬렉션에서 첫 번째 사용자를 찾거나 생성
-//   User.findFirstUser()
-//     .then((user) => {
-//       if (user) {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         console.log("Using existing user:", user._id);
-//         next();
-//       } else {
-//         // 사용자가 없으면 새로 생성
-//         console.log("No users found, creating new user...");
-//         const newUser = new User("Test User", "test@example.com", {
-//           items: [],
-//         });
-//         return newUser.save().then((result) => {
-//           // 새로 생성된 사용자
-//           const user = result.ops[0];
-//           req.user = new User(user.name, user.email, user.cart, user._id);
-//           console.log("New user created:", user._id);
-//           next();
-//         });
-//       }
-//     })
-//     .catch((err) => {
-//       console.log("Error with user:", err);
-//       req.user = null;
-//       next();
-//     });
-// });
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("68c6afac3a7553e109592107");
+    req.user = user;
+    next();
+  } catch (err) {
+    console.errror(err);
+  }
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(get404);
 
-mongoose
-  .connect(DB_URL)
-  .then((result) => {
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log("Connected to MongoDB");
+
+    const user = await User.findOne();
+    if (!user) {
+      const newUser = new User({
+        name: "Max",
+        email: "max@test.com",
+        cart: {
+          items: [],
+        },
+      });
+      await newUser.save();
+      console.log("Default user created");
+    }
+
     app.listen(4000);
-  })
-  .catch((err) => console.error(err));
+    console.log("Server running on port 4000");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+startServer();
--- models/user.js
+++ models/user.js
@@ -1,3 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: mongoose.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
+
 // import mongodb from "mongodb";
 // import { getDb } from "../utils/database.js";
 
