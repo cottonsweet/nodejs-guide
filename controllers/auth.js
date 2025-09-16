bfd7e2287a6cd58336f8fe9a95dca31d0ab9bb56
lk1 <whitecandy777@naver.com>
2025-09-16 21:53:03 +0900

    feat: 쿠키 구성하기

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,10 @@
 export const getLogin = async (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
+  // const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
   try {
     res.render("auth/login", {
       path: "/login",
       pageTitle: "Login",
-      isAuthenticated: isLoggedIn,
+      isAuthenticated: false,
     });
   } catch (err) {
     console.error(err);
@@ -12,6 +12,6 @@ export const getLogin = async (req, res, next) => {
 };
 
 export const postLogin = async (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/");
 };
